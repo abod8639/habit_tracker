14b02d7825ee998a0f7dcd00ddce7468603d292c
abot8639 <abot8639@gmail.com>
2026-09-23 08:51:38 +0300

    feat: add reusable AppConfirmationDialog and update logout flow and UI components

--- lib/features/setting/presentation/widget/logout_tile.dart
+++ lib/features/setting/presentation/widget/logout_tile.dart
@@ -5,20 +5,36 @@ import 'package:habit_tracker/features/setting/presentation/widget/animated_sett
 import 'package:habit_tracker/core/functions/show_logout_dialog.dart';
 import 'package:habit_tracker/generated/l10n.dart';
 
-Widget buildLogoutTile(AnimationController animationController) {
-  final authController = Get.find<AuthController>();
-  final user = authController.currentUser;
-  if (user != null) {
+
+
+class LogoutTile extends StatelessWidget {
+  final AnimationController animationController;
+
+  const LogoutTile({super.key, required this.animationController});
+
+  @override
+  Widget build(BuildContext context) {
+    final authController = Get.find<AuthController>();
+    final user = authController.currentUser;
+    if (user == null) {
+      return const SizedBox.shrink();
+    }
+
+    final themeColors = Theme.of(context).colorScheme;
+
     return AnimatedSettingTile(
       animationController: animationController,
       index: 2,
-    icon: Icons.logout_rounded,
-    title: S.current.logout,
-    subtitle: S.current.logoutFromAccount,
-    textColor: Colors.red,
-    onTap: () => showLogoutDialog(authController),
-  );
-  }else{
-    return SizedBox.shrink();
+      icon: Icons.logout_rounded,
+      title: S.current.logout,
+      subtitle: S.current.logoutFromAccount,
+      textColor: themeColors.error,
+      onTap: () => showLogoutDialog(authController, context),
+    );
   }
 }
+
+Widget buildLogoutTile(AnimationController animationController) {
+  return LogoutTile(animationController: animationController);
+}
+
