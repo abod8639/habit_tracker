33ef5d8f854da63fad5dbf15fd157f61e4a0d999
abot8639 <abot8639@gmail.com>
2026-09-23 03:18:54 +0300

    feat: expand localization support and improve language controller locale handling

--- lib/features/setting/presentation/controllers/lang_controller.dart
+++ lib/features/setting/presentation/controllers/lang_controller.dart
@@ -1,16 +1,29 @@
 import 'dart:ui';
-// import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import '../../domain/usecases/get_language_usecase.dart';
 import '../../domain/usecases/save_language_usecase.dart';
 import 'package:habit_tracker/features/setting/data/datasources/lang_storage.dart';
+import 'package:habit_tracker/generated/l10n.dart';
 
 class LangController extends GetxController {
   final GetLanguageUseCase _getLanguageUseCase = Get.find();
   final SaveLanguageUseCase _saveLanguageUseCase = Get.find();
 
-  var language = Intl.getCurrentLocale().obs;
+  var language = LangStorage.defaultLanguage.obs;
+
+  Locale get effectiveLocale {
+    final code = language.value;
+    if (code == 'ar') return const Locale('ar');
+    if (code == 'en') return const Locale('en');
+    final deviceLocale = PlatformDispatcher.instance.locale;
+    if (deviceLocale.languageCode == 'ar') {
+      return const Locale('ar');
+    }
+    return const Locale('en');
+  }
+
+  bool get isArabic => effectiveLocale.languageCode == 'ar';
 
   @override
   void onInit() {
@@ -22,12 +35,12 @@ class LangController extends GetxController {
     final result = await _getLanguageUseCase();
     result.fold(
       (failure) {
-        // debugPrint('Error loading language: ${failure.message}');
         language.value = LangStorage.defaultLanguage;
+        _applyLocale(effectiveLocale);
       },
       (langCode) {
         language.value = langCode;
-        Get.updateLocale(Locale(langCode));
+        _applyLocale(effectiveLocale);
       },
     );
   }
@@ -38,10 +51,16 @@ class LangController extends GetxController {
       (failure) {
         // debugPrint('Error saving language: ${failure.message}');
       },
-      (_) {
+      (_) async {
         language.value = lang;
-        Get.updateLocale(Locale(lang));
+        await _applyLocale(effectiveLocale);
       },
     );
+  }
+
+  Future<void> _applyLocale(Locale locale) async {
+    await S.load(locale);
+    Intl.defaultLocale = locale.languageCode;
+    Get.updateLocale(locale);
   }
 }
